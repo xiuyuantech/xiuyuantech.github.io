e77553a46feabb850760ff7c2d14866e989c460f
xiuyuantech <362685126@qq.com>
2026-07-12 11:13:39 +0800

    Site updated: 2026-07-12 11:13:37

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11270374
+    "zoneId": 11272884
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
